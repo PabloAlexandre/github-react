bb55f8d8180b6197759c8706e4d964b843da8895
Pablo Alexandre <=>
2018-01-25 03:38:08 -0200

    fix: adding server to render page

---
+++ server/index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var path = require('path')
+
+var app = express()
+app.use(express.static(__dirname))
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+var PORT = process.env.PORT || 8080
+app.listen(PORT)
